7218558e939b1568c40fbb3022bb8c77e8efc2db
Joseph Liccini <joseph.liccini@ufl.edu>
2015-03-14 18:56:46 -0400

    clean up a little

--- main.c
+++ main.c
@@ -5,30 +5,26 @@
 #include "ast.h"
 
 extern int yyparse();
-extern int yylex();
 extern int yy_scan_string(const char *);
-extern int yy_delete_buffer(int buf);
+//extern int yy_delete_buffer(int buf);
 extern struct AstRoot* astRoot;
 
 int main(int argc, char* argv[]) {
-	//char* cmd;
 	char line[MAX_LENGTH];
 
-	while(1) {
+	while (1) {
 		printf("jsh> ");
-		if (!fgets(line, MAX_LENGTH, stdin)) break;
-		//system(line);
-		
+		if (!fgets(line, MAX_LENGTH, stdin))
+			break;
+
 		int buf = yy_scan_string(line);
 		int returnVal = yyparse();
-    if (returnVal == 0) {
-      if (astRoot != NULL) {
-        executeAstRoot(astRoot);
-      }
-    }
+		if (returnVal == 0) {
+			if (astRoot != NULL) {
+				executeAstRoot(astRoot);
+			}
+		}
 		//yy_delete_buffer(buf);
-		//printf("The return value of yyparse() is: %d\n", returnVal);
-
 	}
 	return 0;
 }
