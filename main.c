15018918cb7a9f3807347d8721bdddac7ef371c0
Jean-Ralph Aviles <jeanralph.aviles@gmail.com>
2015-03-26 11:15:38 -0400

    Disabled ctrl-[ ctrl-\ ctrl-Z

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,25 +7,35 @@
 
 extern int yyparse();
 extern int yy_scan_string(const char *);
-//extern int yy_delete_buffer(int buf);
 extern struct AstRoot* astRoot;
 
+void init(void);
+
 int main(int argc, char* argv[]) {
-	char line[MAX_LENGTH];
+  char line[MAX_LENGTH];
+  init();
 
-	while (1) {
-		printf("jsh> ");
-		if (!fgets(line, MAX_LENGTH, stdin))
-			break;
+  while (1) {
+    printf("jsh> ");
+    if (!fgets(line, MAX_LENGTH, stdin)) {
+      break;
+    }
 
-		int buf = yy_scan_string(line);
-		int returnVal = yyparse();
-		if (returnVal == 0) {
-			if (astRoot != NULL) {
-				executeAstRoot(astRoot);
-			}
-		}
-		//yy_delete_buffer(buf);
-	}
-	return 0;
+    int buf = yy_scan_string(line);
+    int returnVal = yyparse();
+    if (returnVal == 0) {
+      if (astRoot != NULL) {
+        executeAstRoot(astRoot);
+      }
+    } else {
+      printf("An error occurred\n");
+    }
+  }
+  return 0;
+}
+
+void init(void) {
+  signal(SIGINT, SIG_IGN);  /* disable ctrl-C */
+  signal(SIGQUIT, SIG_IGN); /* disable ctrl-\ */
+  signal(SIGTSTP, SIG_IGN); /* disable ctrl-Z */
 }
