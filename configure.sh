484762b091233dd43b6b20c0a1833957dd677ab4
jeanralphaviles <jeanralph.aviles@gmail.com>
2015-04-09 14:38:06 -0400

    Increased configure script efficiency

--- configure.sh
+++ configure.sh
@@ -1,7 +1,14 @@
 #!bin/bash
 READLINELINK="ftp://ftp.cwru.edu/pub/bash/readline-6.3.tar.gz"
-wget $READLINELINK
-tar -xvf readline-6.3.tar.gz && mv readline-6.3 readline
+
+if [ ! -d "readline" ]; then
+    # Control will enter here if $DIRECTORY doesn't exist.
+    if [ ! -f "readline-6.3.tar.gz" ]; then
+      wget $READLINELINK
+    fi
+    tar -xvf readline-6.3.tar.gz && mv readline-6.3 readline
+fi
+
 cd readline && ./configure --prefix=$PWD && make && make install
 cd ..
 make
