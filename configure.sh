626a9405fcb9cadfde457eb356979ea5702532cc
Joseph Liccini <joseph.liccini@ufl.edu>
2015-04-09 20:15:13 -0400

    Added README.pdf

--- configure.sh
+++ configure.sh
@@ -3,7 +3,7 @@ READLINELINK="ftp://ftp.cwru.edu/pub/bash/readline-6.3.tar.gz"
 
 if [ ! -d "readline" ]; then
     # Control will enter here if $DIRECTORY doesn't exist.
-    if [ ! -f "readline-6.3.tar.gz" ]; then
+    if [ ! -e "readline-6.3.tar.gz" ]; then
       wget $READLINELINK
     fi
     tar -xvf readline-6.3.tar.gz && mv readline-6.3 readline
